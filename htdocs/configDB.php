234fa1777b047b8d251f629bfe6cbcbc4c04b805
Pavel Deryugin <91695194+ShadowMadness792@users.noreply.github.com>
2021-12-29 16:00:48 +0300

    Delete htdocs directory

--- htdocs/configDB.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $dsn = 'mysql:host=localhost;dbname=stom';
-  $pdo = new PDO($dsn, 'root', 'root');
-?>
--- htdocs/delete_doctor.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require 'configDB.php';
-$id = $_GET['id'];
-$query1 = $pdo->query("DELETE FROM `doctors` WHERE `doctor_id`= '$id'");
-
-header('Location: /doctors.php');
-?>
